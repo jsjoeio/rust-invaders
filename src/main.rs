09da467999281a7d45d244cd6b6a30733e266104
Joe Previte <jjprevite@gmail.com>
2022-06-07 14:12:29 +0000

    wip: move ferris to bottom

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,29 @@ fn main() {
         .run()
 }
 
-fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut windows: ResMut<Windows>,
+) {
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    // add rectangle
+    // capture window size
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+
+    // position window (for tutorial)
+    window.set_position(IVec2::new(2780, 4900));
+
+    // add player
+    let bottom = -win_h / 2.;
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load(PLAYER_SPRITE),
+        transform: Transform {
+            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. + 5., 10.),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
